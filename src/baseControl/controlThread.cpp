robotology/navigation

commit 58369a5441d97a9d78da60fc6abfaddf5fb816e9

code cleanup

src/baseControl/controlThread.cpp
@@ -23,6 +23,78 @@
 #include "cer/cer_motors.h"
 #include "ikart/ikart_motors.h"
 
+void ControlThread::afterStart(bool s)
+{
+    if (s)
+        yInfo("Control thread started successfully");
+    else
+        yError("Control thread did not start");
+}
+
+void ControlThread::threadRelease()
+{
+    if (odometry_handler)  {delete odometry_handler; odometry_handler=0;}
+    if (motor_handler)     {delete motor_handler; motor_handler=0;}
+    if (input_handler)     {delete input_handler; input_handler = 0; }
+
+    if (linear_speed_pid)  {delete linear_speed_pid;  linear_speed_pid=0;}
+    if (angular_speed_pid) {delete angular_speed_pid; angular_speed_pid=0;}
+    if (linear_ol_pid)     {delete linear_ol_pid;  linear_ol_pid=0;}
+    if (angular_ol_pid)    {delete angular_ol_pid; angular_ol_pid=0;}
+
+    if (debug_enabled)
+    {
+        port_debug_linear.interrupt();
+        port_debug_linear.close();
+        port_debug_angular.interrupt();
+        port_debug_angular.close();
+    }
+
+    if (rosNode)
+    {
+        rosNode->interrupt();
+        delete rosNode;
+        rosNode = 0;
+    }
+}
+
+double ControlThread::get_max_linear_vel()  { return max_linear_vel; }
+double ControlThread::get_max_angular_vel() { return max_angular_vel; }
+
+ControlThread::ControlThread (unsigned int _period, ResourceFinder &_rf, Property options) : RateThread(_period), rf(_rf), ctrl_options(options)
+{
+    rosNode                  = NULL;
+    control_board_driver     = 0;
+    thread_timeout_counter   = 0;
+    base_control_type        = BASE_CONTROL_NONE;
+
+    input_filter_enabled     = 0;
+    lin_ang_ratio            = 0.7;
+    robot_type               = ROBOT_TYPE_NONE;
+
+    debug_enabled            = false;
+    both_lin_ang_enabled     = true;
+    thread_period            = _period;
+
+    input_linear_speed       = 0;
+    input_angular_speed      = 0;
+    input_desired_direction  = 0;
+    input_pwm_gain           = 0;
+    linear_speed_pid         = 0;
+    angular_speed_pid        = 0;
+    linear_ol_pid            = 0;
+    angular_ol_pid           = 0;
+    max_linear_vel           = 0;
+    max_angular_vel          = 0;
+    max_angular_acc          = 0;
+    max_linear_acc           = 0;
+    remoteName               = ctrl_options.find("remote").asString();
+    localName                = ctrl_options.find("local").asString();
+    odometry_handler         = 0;
+    motor_handler            = 0;
+    input_handler            = 0;
+}
+
 void ControlThread::apply_ratio_limiter (double& linear_speed, double& angular_speed)
 {
     if (lin_ang_ratio>1.0)  lin_ang_ratio = 1.0;
@@ -218,7 +290,7 @@ void ControlThread::run()
     double pidout_direction     = 0;
 
     //read inputs (input_linear_speed in m/s, input_angular_speed in deg/s...)
-    this->input_handler->read_inputs(&input_linear_speed, &input_angular_speed, &input_desired_direction, &input_pwm_gain);
+    /*this->input_handler->read_inputs(&input_linear_speed, &input_angular_speed, &input_desired_direction, &input_pwm_gain);
 
     if (input_linear_speed < 0)
     {
@@ -247,7 +319,7 @@ void ControlThread::run()
     
     //apply ratio limiter
     if (ratio_limiter_enabled) apply_ratio_limiter(input_linear_speed, input_angular_speed);
-
+    */
     /*
     if (!lateral_movement_enabled)
     {
@@ -258,7 +330,7 @@ void ControlThread::run()
     */
 
     //The controllers
-    if (base_control_type == BASE_CONTROL_OPENLOOP_NO_PID)
+    /*if (base_control_type == BASE_CONTROL_OPENLOOP_NO_PID)
     {
         double exec_pwm_gain = input_pwm_gain / 100.0 * 1.0;
         //the following /2 is used to avoid saturation due to decoupling
@@ -295,7 +367,17 @@ void ControlThread::run()
     {
         yError ("Unknown control mode!");
         this->motor_handler->execute_none();
-    }
+    }*/
+    
+    yDebug("BEFORE");
+ //   this->motor_handler->ivel->velocityMove(0,0.0);
+ //   this->motor_handler->ivel->velocityMove(1,0.0);
+        double sps [2];
+    sps[0]=0;
+    sps[1]=0;
+   this->motor_handler->ivel->velocityMove(sps);
+    yDebug("AFTER");
+    
 }
 
 void ControlThread::printStats()
@@ -376,6 +458,7 @@ bool ControlThread::threadInit()
 
     control_board_options.put("remote", remoteName.c_str());
     control_board_options.put("local", localName.c_str());
+    control_board_options.put("writeStrict","on");
 
     do
     {

src/baseControl/controlThread.h
@@ -121,55 +121,9 @@ public:
     Input* const         get_input_handler()    { return input_handler; }
     void                 enable_debug(bool b);
 
-    ControlThread
-    (
-        unsigned int _period, 
-        ResourceFinder &_rf,
-        Property options
-    )
-    : RateThread(_period), rf(_rf), ctrl_options(options)
-    {
-        rosNode                  = NULL;
-        control_board_driver     = 0;
-        thread_timeout_counter   = 0;
-        base_control_type        = BASE_CONTROL_NONE;
-
-        input_filter_enabled     = 0;
-        lin_ang_ratio            = 0.7;
-        robot_type               = ROBOT_TYPE_NONE;
-
-        debug_enabled            = false;
-        both_lin_ang_enabled     = true;
-        thread_period            = _period;
-
-        input_linear_speed       = 0;
-        input_angular_speed      = 0;
-        input_desired_direction  = 0;
-        input_pwm_gain           = 0;
-        linear_speed_pid         = 0;
-        angular_speed_pid        = 0;
-        linear_ol_pid            = 0;
-        angular_ol_pid           = 0;
-        max_linear_vel           = 0;
-        max_angular_vel          = 0;
-        max_angular_acc          = 0;
-        max_linear_acc           = 0;
-        remoteName               = ctrl_options.find("remote").asString();
-        localName                = ctrl_options.find("local").asString();
-        odometry_handler         = 0;
-        motor_handler            = 0;
-        input_handler            = 0;
-    }
-
+    ControlThread   (unsigned int _period, ResourceFinder &_rf, Property options);
     virtual bool threadInit();
-
-    virtual void afterStart(bool s)
-    {
-        if (s)
-            yInfo("Control thread started successfully");
-        else
-            yError("Control thread did not start");
-    }
+    virtual void afterStart(bool s);
 
     virtual void run();
     bool set_control_type (string s);
@@ -184,35 +138,10 @@ public:
     
     void apply_control_openloop_pid(double& pidout_linear_throttle, double& pidout_angular_throttle, const double ref_linear_speed, const double ref_angular_speed);
     void apply_control_speed_pid(double& pidout_linear_throttle, double& pidout_angular_throttle, const double ref_linear_speed, const double ref_angular_speed);
-    double get_max_linear_vel()  { return max_linear_vel; }
-    double get_max_angular_vel() { return max_angular_vel; }
+    double get_max_linear_vel();
+    double get_max_angular_vel();
 
-    virtual void threadRelease()
-    {
-        if (odometry_handler)  {delete odometry_handler; odometry_handler=0;}
-        if (motor_handler)     {delete motor_handler; motor_handler=0;}
-        if (input_handler)     {delete input_handler; input_handler = 0; }
-
-        if (linear_speed_pid)  {delete linear_speed_pid;  linear_speed_pid=0;}
-        if (angular_speed_pid) {delete angular_speed_pid; angular_speed_pid=0;}
-        if (linear_ol_pid)     {delete linear_ol_pid;  linear_ol_pid=0;}
-        if (angular_ol_pid)    {delete angular_ol_pid; angular_ol_pid=0;}
-
-        if (debug_enabled)
-        {
-            port_debug_linear.interrupt();
-            port_debug_linear.close();
-            port_debug_angular.interrupt();
-            port_debug_angular.close();
-        }
-
-        if (rosNode)
-        {
-            rosNode->interrupt();
-            delete rosNode;
-            rosNode = 0;
-        }
-    }
+    virtual void threadRelease();
 };
 
 #endif

src/iKartNav/Navigator.cpp
@@ -6,6 +6,199 @@
 #define PRIO_LASER  1
 #define PRIO_VISION 2
 
+void Navigator::updateOdometry(Vec2D P,double H)
+{
+    mOdoP=P;
+    mOdoH=H;
+
+    int xc=XWorld2Grid(mOdoP.x);
+    int yc=YWorld2Grid(mOdoP.y);
+
+    if (xc> DIM_BY_2) shiftMapSouth();
+    if (xc<-DIM_BY_2) shiftMapNorth();
+
+    if (yc> DIM_BY_2) shiftMapEast();
+    if (yc<-DIM_BY_2) shiftMapWest();
+}
+
+void Navigator::setOmega(double omega)
+{
+    if (omega>mMaxOmega)
+    {
+        mOmegaRef=mMaxOmega;
+    }
+    else if (omega<-mMaxOmega)
+    {
+        mOmegaRef=-mMaxOmega;
+    }
+    else
+    {
+        mOmegaRef=omega;
+    }
+}
+
+void Navigator::setVisionTarget(double heading)
+{
+    int angle=RAYS_BY_2+int(0.5+4.0*mod180(heading));
+        
+    mTargetFilter.addPoint(mOdoP,Vec2D(heading+mOdoH),mRanges[angle]);
+
+    if (mTargetFilter.numSamples()<2)
+    {
+        printf("Vision target not complete (%d samples)\n",mTargetFilter.numSamples());
+
+        mHaveTarget=false;            
+        return;
+    }
+        
+    mTarget=mTargetFilter.getTarget();
+        
+    printf("Vision target X=%.3f Y=%.3f\n",mTarget.x,mTarget.y);
+    
+    replaceTarget(mTarget);
+}
+
+void Navigator::setUserTarget(double heading,double distance)
+{
+    mHaveTarget=true;
+    mTarget=mOdoP+distance*Vec2D(mOdoH+heading);
+    printf("NEW TARGET X=%.3f Y=%.3f\n",mTarget.x,mTarget.y);
+    fflush(stdout);
+    
+    replaceTarget(mTarget);
+}
+    
+void Navigator::sendCtrlRef()
+{
+    double timeNew=yarp::os::Time::now();
+    static double timeOld=timeNew;  
+    double step=timeNew-timeOld;
+    timeOld=timeNew;
+
+    if (mVelRef!=mVel)
+    {
+        Vec2D Verr=mVelRef-mVel;
+
+        if (Verr.mod()>step*mLinAcc) mVel+=Verr.norm(step*mLinAcc); else mVel=mVelRef;
+    }
+
+    if (mOmegaRef!=mOmega)
+    {
+        double Werr=mOmegaRef-mOmega;
+
+        if (Werr>step*mRotAcc) mOmega+=step*mRotAcc; else if (Werr<-step*mRotAcc) mOmega-=step*mRotAcc; else mOmega=mOmegaRef;
+    }
+
+    mKartCtrl->setCtrlRef(-mVel.arg(),mVel.mod(),-mOmega,NAVIGATOR_PRIORITY);
+}
+
+void Navigator::sendBrake()
+{
+    mOmega=mOmegaRef=0.0;
+    mVel=mVelRef=Vec2D::zero;
+    mKartCtrl->setCtrlRef(0.0,0.0,0.0,NAVIGATOR_PRIORITY);
+}
+
+void Navigator::setVel(const Vec2D& vel)
+{
+    if (vel.mod()>mMaxSpeed)
+    {
+        mVelRef=vel.norm(mMaxSpeed);
+    }
+    else
+    {
+        mVelRef=vel;
+    }
+}
+
+void Navigator::replaceTarget(Vec2D& target)
+{
+    static double timeOld=0.0;
+
+    int xr=XWorld2GridRound(target.x);
+    int yr=YWorld2GridRound(target.y);
+
+    if (xr<=-DIM || xr>=DIM || yr<=-DIM || yr>=DIM) return;
+
+    Vec2D direction,gradient;
+    double curvature,zeta;
+
+    Vec2D X=target;
+    Vec2D D=mOdoP-target;
+
+    int n=(int)(100.0*D.mod());
+
+    mHaveTarget=false;
+
+    for (int i=0; i<n; ++i)
+    {
+        int result=followGNF(X,direction,curvature,zeta,gradient);
+
+        if (result==GNF_OUT_OF_GRID)
+        {
+            printf("WARNING: the target is out of grid\n");
+            mHaveTarget=true;
+                
+            if (mTargetPortO.getOutputCount()>0)
+            {
+                double timeNew=yarp::os::Time::now();
+
+                if (timeNew-timeOld>1.0)
+                {
+                    timeOld=timeNew;
+                    D=target-mOdoP;
+                    yarp::os::Bottle msg;
+                    msg.addDouble(-D.arg());
+                    msg.addDouble( D.mod());
+                    mTargetPortO.write(msg);
+                }
+            }
+
+            return;
+        }
+
+        if (zeta<THR)
+        {
+            if (i)
+            {
+                if (!mHaveTargetH)
+                {
+                    mHaveTargetH=true;
+                    mTargetH=(target-X).arg();
+                }
+
+                printf("Target replaced X=%.3f  Y=%.3f\n",X.x,X.y);
+                fflush(stdout);
+            }
+
+            mHaveTarget=true;
+            target=X;
+
+            if (mTargetPortO.getOutputCount()>0)
+            {
+                double timeNew=yarp::os::Time::now();
+
+                if (timeNew-timeOld>1.0)
+                {
+                    timeOld=timeNew;
+                    D=target-mOdoP;
+                    yarp::os::Bottle msg;
+                    msg.addDouble(-D.arg());
+                    msg.addDouble( D.mod());
+                    mTargetPortO.write(msg);
+                }
+            }
+                
+            return;
+        }
+
+        X+=(gradient*D>=0.0?0.01:-0.01)*gradient;
+    }
+
+    printf("Can't replace target in a free space position (Z=%f - Zmax=%f)\n",zeta,THR);
+    fflush(stdout);
+}
+
 Navigator::Navigator(yarp::os::ResourceFinder *rf)
     :
         RateThread(10),

src/iKartNav/Navigator.h
@@ -34,83 +34,16 @@ public:
     virtual void run();
     virtual void threadRelease();
     virtual void onStop();
-
-    void updateOdometry(Vec2D P,double H)
-    {
-        mOdoP=P;
-        mOdoH=H;
-
-        int xc=XWorld2Grid(mOdoP.x);
-        int yc=YWorld2Grid(mOdoP.y);
-
-        if (xc> DIM_BY_2) shiftMapSouth();
-        if (xc<-DIM_BY_2) shiftMapNorth();
-
-        if (yc> DIM_BY_2) shiftMapEast();
-        if (yc<-DIM_BY_2) shiftMapWest();
-    }
+    void updateOdometry(Vec2D P,double H);
 
 protected:
-    int abs(int x){ return x>0?x:-x; }
+    inline int abs(int x){ return x>0?x:-x; }
+    void setOmega(double omega);
 
-    void setOmega(double omega)
-    {
-        if (omega>mMaxOmega)
-        {
-            mOmegaRef=mMaxOmega;
-        }
-        else if (omega<-mMaxOmega)
-        {
-            mOmegaRef=-mMaxOmega;
-        }
-        else
-        {
-            mOmegaRef=omega;
-        }
-    }
+    void setVel(const Vec2D& vel);
 
-    void setVel(const Vec2D& vel)
-    {
-        if (vel.mod()>mMaxSpeed)
-        {
-            mVelRef=vel.norm(mMaxSpeed);
-        }
-        else
-        {
-            mVelRef=vel;
-        }
-    }
-
-    void sendBrake()
-    {
-        mOmega=mOmegaRef=0.0;
-        mVel=mVelRef=Vec2D::zero;
-        mKartCtrl->setCtrlRef(0.0,0.0,0.0,NAVIGATOR_PRIORITY);
-    }
-
-    void sendCtrlRef()
-    {
-        double timeNew=yarp::os::Time::now();
-        static double timeOld=timeNew;  
-        double step=timeNew-timeOld;
-        timeOld=timeNew;
-
-        if (mVelRef!=mVel)
-        {
-            Vec2D Verr=mVelRef-mVel;
-
-            if (Verr.mod()>step*mLinAcc) mVel+=Verr.norm(step*mLinAcc); else mVel=mVelRef;
-        }
-
-        if (mOmegaRef!=mOmega)
-        {
-            double Werr=mOmegaRef-mOmega;
-
-            if (Werr>step*mRotAcc) mOmega+=step*mRotAcc; else if (Werr<-step*mRotAcc) mOmega-=step*mRotAcc; else mOmega=mOmegaRef;
-        }
-
-        mKartCtrl->setCtrlRef(-mVel.arg(),mVel.mod(),-mOmega,NAVIGATOR_PRIORITY);
-    }
+    void sendBrake();
+    void sendCtrlRef();
 
     inline int XWorld2Grid(double& x){ return int(10.0*x)-mOx; }
     inline int YWorld2Grid(double& y){ return int(10.0*y)-mOy; }
@@ -122,125 +55,9 @@ protected:
     inline double YGrid2World(int y){ return 0.1*double(y+mOy); }
     inline Vec2D Grid2World(int x,int y) { return Vec2D(0.1*double(x+mOx),0.1*double(y+mOy)); }
 
-    void setVisionTarget(double heading)
-    {
-        int angle=RAYS_BY_2+int(0.5+4.0*mod180(heading));
-        
-        mTargetFilter.addPoint(mOdoP,Vec2D(heading+mOdoH),mRanges[angle]);
-
-        if (mTargetFilter.numSamples()<2)
-        {
-            printf("Vision target not complete (%d samples)\n",mTargetFilter.numSamples());
-
-            mHaveTarget=false;            
-            return;
-        }
-        
-        mTarget=mTargetFilter.getTarget();
-        
-        printf("Vision target X=%.3f Y=%.3f\n",mTarget.x,mTarget.y);
-    
-        replaceTarget(mTarget);
-    }    
-
-    void setUserTarget(double heading,double distance)
-    {
-        mHaveTarget=true;
-        mTarget=mOdoP+distance*Vec2D(mOdoH+heading);
-        printf("NEW TARGET X=%.3f Y=%.3f\n",mTarget.x,mTarget.y);
-        fflush(stdout);
-    
-        replaceTarget(mTarget);
-    }
-    
-    void replaceTarget(Vec2D& target) 
-    {
-        static double timeOld=0.0;
-
-        int xr=XWorld2GridRound(target.x);
-        int yr=YWorld2GridRound(target.y);
-
-        if (xr<=-DIM || xr>=DIM || yr<=-DIM || yr>=DIM) return;
-
-        Vec2D direction,gradient;
-        double curvature,zeta;
-
-        Vec2D X=target;
-        Vec2D D=mOdoP-target;
-
-        int n=(int)(100.0*D.mod());
-
-        mHaveTarget=false;
-
-        for (int i=0; i<n; ++i)
-        {
-            int result=followGNF(X,direction,curvature,zeta,gradient);
-
-            if (result==GNF_OUT_OF_GRID)
-            {
-                printf("WARNING: the target is out of grid\n");
-                mHaveTarget=true;
-                
-                if (mTargetPortO.getOutputCount()>0)
-                {
-                    double timeNew=yarp::os::Time::now();
-
-                    if (timeNew-timeOld>1.0)
-                    {
-                        timeOld=timeNew;
-                        D=target-mOdoP;
-                        yarp::os::Bottle msg;
-                        msg.addDouble(-D.arg());
-                        msg.addDouble( D.mod());
-                        mTargetPortO.write(msg);
-                    }
-                }
-
-                return;
-            }
-
-            if (zeta<THR)
-            {
-                if (i)
-                {
-                    if (!mHaveTargetH)
-                    {
-                        mHaveTargetH=true;
-                        mTargetH=(target-X).arg();
-                    }
-
-                    printf("Target replaced X=%.3f  Y=%.3f\n",X.x,X.y);
-                    fflush(stdout);
-                }
-
-                mHaveTarget=true;
-                target=X;
-
-                if (mTargetPortO.getOutputCount()>0)
-                {
-                    double timeNew=yarp::os::Time::now();
-
-                    if (timeNew-timeOld>1.0)
-                    {
-                        timeOld=timeNew;
-                        D=target-mOdoP;
-                        yarp::os::Bottle msg;
-                        msg.addDouble(-D.arg());
-                        msg.addDouble( D.mod());
-                        mTargetPortO.write(msg);
-                    }
-                }
-                
-                return;
-            }
-
-            X+=(gradient*D>=0.0?0.01:-0.01)*gradient;
-        }
-
-        printf("Can't replace target in a free space position (Z=%f - Zmax=%f)\n",zeta,THR);
-        fflush(stdout);
-    }
-    
+    void setVisionTarget(double heading);
+    void setUserTarget(double heading,double distance);
+    void replaceTarget(Vec2D& target);
     void addEvent(double heading,double distance,double radius);
 
     void updateMap(yarp::sig::Vector& rangeData);

src/iKartNav/TargetFilter.cpp
@@ -1,0 +1,116 @@
+#include "TargetFilter.h"
+#include "yarp/os/Log.h"
+
+TargetFilter::TargetFilter()
+{
+    mNumSamples=0;
+    Q=Vec2D(100.0,100.0);
+}
+
+TargetFilter::~TargetFilter()
+{
+}
+
+void TargetFilter::addPoint(Vec2D P,Vec2D U,double maxRange)
+{
+    if (maxRange==0.0) return;
+
+    if ((P-Q).mod2()<0.16) return;
+
+    if (mNumSamples==0)
+    {
+        yDebug("0 samples");
+            
+        Q=P; W=U;
+        mTarget=P+maxRange*U;
+        mNumSamples=1;
+        return;
+    }
+
+    if (mNumSamples==1)
+    {
+        yDebug("1 sample");
+        
+        double k=W*U;
+        double d=1.0-k*k;
+
+        if (d==0.0)
+        {
+            Q=P; W=U;
+            mTarget=P+maxRange*U;
+            return;
+        }
+
+        d=1.0/d;
+        double t=((Q-P)*(U-k*W))*d;
+        double s=((P-Q)*(W-k*U))*d;
+
+        Q=P; W=U;
+            
+        if (t<0.5 || s<0.5 || t>15.0 || s>15.0)
+        {
+            mTarget=P+maxRange*U;
+            return;
+        }
+
+        mTarget=P+t*U;
+        mNumSamples=2;
+        return;
+    }
+
+    if (mNumSamples==2)
+    {
+        yDebug("2 samples");
+            
+        double k=W*U;
+        double d=1.0-k*k;
+
+        if (d==0.0)
+        {
+            Q=P; W=U;
+            mTarget=P+maxRange*U;
+            mNumSamples=1;
+            return;
+        }
+
+        d=1.0/d;
+        double t=((Q-P)*(U-k*W))*d;
+        double s=((P-Q)*(W-k*U))*d;
+
+        Q=P; W=U;
+            
+        if (t<0.5 || s<0.5 || t>15.0 || s>15.0)
+        {
+            mTarget=P+maxRange*U;
+            mNumSamples=1;
+            return;
+        }
+
+        Vec2D newTarget=P+t*U;
+
+        if ((newTarget-mTarget).mod2()>1.0)
+        {
+            mTarget=P+maxRange*U;
+            mNumSamples=1;
+            return;
+        }
+
+        mTarget=0.75*mTarget+0.25*newTarget;
+    }
+}
+
+int TargetFilter::numSamples()
+{
+    return mNumSamples;
+}
+
+void TargetFilter::reset()
+{
+    mNumSamples=0;
+}
+
+Vec2D TargetFilter::getTarget()
+{
+    return mTarget;
+}
+

src/iKartNav/TargetFilter.h
@@ -6,124 +6,17 @@
 class TargetFilter
 {
 public:
+    TargetFilter();
+    ~TargetFilter();
 
-    TargetFilter(void)
-    {
-        mNumSamples=0;
-        Q=Vec2D(100.0,100.0);
-    }
-
-    ~TargetFilter(void)
-    {
-    }
-
-    void addPoint(Vec2D P,Vec2D U,double maxRange)
-    {
-        if (maxRange==0.0) return;
-
-        if ((P-Q).mod2()<0.16) return;
-
-        if (mNumSamples==0)
-        {
-            printf("0 samples\n");
-            
-            Q=P; W=U;
-            mTarget=P+maxRange*U;
-            mNumSamples=1;
-            return;
-        }
-
-        if (mNumSamples==1)
-        {
-            printf("1 sample\n");
-        
-            double k=W*U;
-            double d=1.0-k*k;
-
-            if (d==0.0)
-            {
-                Q=P; W=U;
-                mTarget=P+maxRange*U;
-                return;
-            }
-
-            d=1.0/d;
-            double t=((Q-P)*(U-k*W))*d;
-            double s=((P-Q)*(W-k*U))*d;
-
-            Q=P; W=U;
-            
-            if (t<0.5 || s<0.5 || t>15.0 || s>15.0)
-            {
-                mTarget=P+maxRange*U;
-                return;
-            }
-
-            mTarget=P+t*U;
-            mNumSamples=2;
-            return;
-        }
-
-        if (mNumSamples==2)
-        {
-            printf("2 samples\n");
-            
-            double k=W*U;
-            double d=1.0-k*k;
-
-            if (d==0.0)
-            {
-                Q=P; W=U;
-                mTarget=P+maxRange*U;
-                mNumSamples=1;
-                return;
-            }
-
-            d=1.0/d;
-            double t=((Q-P)*(U-k*W))*d;
-            double s=((P-Q)*(W-k*U))*d;
-
-            Q=P; W=U;
-            
-            if (t<0.5 || s<0.5 || t>15.0 || s>15.0)
-            {
-                mTarget=P+maxRange*U;
-                mNumSamples=1;
-                return;
-            }
-
-            Vec2D newTarget=P+t*U;
-
-            if ((newTarget-mTarget).mod2()>1.0)
-            {
-                mTarget=P+maxRange*U;
-                mNumSamples=1;
-                return;
-            }
-
-            mTarget=0.75*mTarget+0.25*newTarget;
-        }
-    }
-
-    int numSamples()
-    {
-        return mNumSamples;
-    }
-
-    void reset()
-    {
-        mNumSamples=0;
-    }
-
-    Vec2D getTarget()
-    {
-        return mTarget;
-    }
+    void addPoint(Vec2D P,Vec2D U,double maxRange);
+    int numSamples();
+    void reset();
+    Vec2D getTarget();
 
 protected:
     Vec2D Q,W;
     Vec2D mTarget;
-
     int mNumSamples;
 };
 

src/iKartNav/Vec2D.cpp
@@ -3,3 +3,61 @@
 const Vec2D Vec2D::zero(0.0,0.0);
 const double Vec2D::DEG2RAD=M_PI/180.0;
 const double Vec2D::RAD2DEG=180.0/M_PI;
+
+double Vec2D::mod2()const
+{
+    return x*x+y*y;
+}
+
+double Vec2D::mod()const
+{
+    return sqrt(x*x+y*y);
+}
+
+Vec2D Vec2D::rot(double alfa)
+{
+    alfa*=DEG2RAD;
+    double cs=cos(alfa);
+    double sn=sin(alfa);
+    return Vec2D(cs*x-sn*y,sn*x+cs*y);
+}
+
+Vec2D Vec2D::rotLeft()const
+{
+    return Vec2D(-y,x);
+}
+
+Vec2D Vec2D::rotRight()const
+{
+    return Vec2D(y,-x);
+}
+
+Vec2D Vec2D::norm(double dl) const
+{ 
+    double dm=mod();
+        
+    if (dm!=0.0)
+        return *this*(dl/dm);
+    else
+        return Vec2D(0.0,0.0);
+}
+
+double Vec2D::normalize(double dl)
+{ 
+    double dm=mod();
+        
+    if (dm!=0.0)
+    {
+        x*=dl/=dm;
+        y*=dl;
+    }
+    else
+        x=y=0.0;
+
+    return dm;
+}
+
+double Vec2D::arg() const
+{
+    return RAD2DEG*atan2(y,x);
+}

src/iKartNav/Vec2D.h
@@ -17,49 +17,14 @@ public:
     }
     ~Vec2D(){}
 
-    double mod2()const{ return x*x+y*y; }
-    double mod()const{ return sqrt(x*x+y*y); }
-
-    Vec2D rot(double alfa)
-    {
-        alfa*=DEG2RAD;
-        double cs=cos(alfa);
-        double sn=sin(alfa);
-        return Vec2D(cs*x-sn*y,sn*x+cs*y);
-    }
-
-    Vec2D rotLeft()const{ return Vec2D(-y,x); }
-    Vec2D rotRight()const{ return Vec2D(y,-x); }
-
-    Vec2D norm(double dl=1.0) const
-    { 
-        double dm=mod();
-        
-        if (dm!=0.0)
-            return *this*(dl/dm);
-        else
-            return Vec2D(0.0,0.0);
-    }
-
-    double normalize(double dl=1.0)
-    { 
-        double dm=mod();
-        
-        if (dm!=0.0)
-        {
-            x*=dl/=dm;
-            y*=dl;
-        }
-        else
-            x=y=0.0;
-
-        return dm;
-    }
-
-    double arg() const
-    {
-        return RAD2DEG*atan2(y,x);
-    }
+    double mod2()const;
+    double mod()const;
+    Vec2D rot(double alfa);
+    Vec2D rotLeft()const;
+    Vec2D rotRight()const;
+    Vec2D norm(double dl=1.0) const;
+    double normalize(double dl=1.0);
+    double arg() const;
 
     inline Vec2D operator+(const Vec2D& p)const{ return Vec2D(x+p.x,y+p.y); }
     inline Vec2D operator-(const Vec2D& p)const{ return Vec2D(x-p.x,y-p.y); }

commit 4f47b79a70450ca5cf706be434910556bd3ed671

fixing issue accidentaly introduced by 58369a5441d97a9d78da60fc6abfaddf5fb816e9

src/baseControl/controlThread.cpp
@@ -290,7 +290,7 @@ void ControlThread::run()
     double pidout_direction     = 0;
 
     //read inputs (input_linear_speed in m/s, input_angular_speed in deg/s...)
-    /*this->input_handler->read_inputs(&input_linear_speed, &input_angular_speed, &input_desired_direction, &input_pwm_gain);
+    this->input_handler->read_inputs(&input_linear_speed, &input_angular_speed, &input_desired_direction, &input_pwm_gain);
 
     if (input_linear_speed < 0)
     {
@@ -319,7 +319,7 @@ void ControlThread::run()
     
     //apply ratio limiter
     if (ratio_limiter_enabled) apply_ratio_limiter(input_linear_speed, input_angular_speed);
-    */
+
     /*
     if (!lateral_movement_enabled)
     {
@@ -330,7 +330,7 @@ void ControlThread::run()
     */
 
     //The controllers
-    /*if (base_control_type == BASE_CONTROL_OPENLOOP_NO_PID)
+    if (base_control_type == BASE_CONTROL_OPENLOOP_NO_PID)
     {
         double exec_pwm_gain = input_pwm_gain / 100.0 * 1.0;
         //the following /2 is used to avoid saturation due to decoupling
@@ -367,17 +367,7 @@ void ControlThread::run()
     {
         yError ("Unknown control mode!");
         this->motor_handler->execute_none();
-    }*/
-    
-    yDebug("BEFORE");
- //   this->motor_handler->ivel->velocityMove(0,0.0);
- //   this->motor_handler->ivel->velocityMove(1,0.0);
-        double sps [2];
-    sps[0]=0;
-    sps[1]=0;
-   this->motor_handler->ivel->velocityMove(sps);
-    yDebug("AFTER");
-    
+}
 }
 
 void ControlThread::printStats()
@@ -458,7 +448,6 @@ bool ControlThread::threadInit()
 
     control_board_options.put("remote", remoteName.c_str());
     control_board_options.put("local", localName.c_str());
-    control_board_options.put("writeStrict","on");
 
     do
     {