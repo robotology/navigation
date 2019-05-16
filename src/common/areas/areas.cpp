3a3bc7c203f6ec7c72c3e9ac0b7d496cfdfad3f3
Marco Randazzo <marco.randazzo@iit.it>
2019-05-16 13:41:49 +0200

    areas: map segmentation improved

--- src/common/areas/areas.cpp
+++ src/common/areas/areas.cpp
@@ -5,10 +5,15 @@
 #include <iostream>
 #include <yarp/dev/MapGrid2D.h>
 #include <yarp/dev/Map2DArea.h>
+#include <yarp/dev/iMap2D.h>
 #include <yarp/math/Vec2D.h>
+#include <yarp/dev/Polydriver.h>
+#include <yarp/os/Logstream.h>
 
 using namespace cv;
 using namespace std;
+using namespace yarp::os;
+using namespace yarp::dev;
 
 const int w = 500;
 int levels = 3;
@@ -18,8 +23,10 @@ static void on_trackbar(int, void*)
 {
     Mat cnt_img = Mat::zeros(w, w, CV_8UC3);
     int _levels = levels - 3;
-    drawContours( cnt_img, contours, 0, Scalar(128,255,255),
-                  3, LINE_AA, hierarchy, std::abs(_levels) );
+    for (auto contour_id = 0; contour_id < contours.size(); contour_id++)
+    {
+        drawContours(cnt_img, contours, contour_id, Scalar(128, 255, 255), 2, LINE_AA, hierarchy, std::abs(_levels));
+    }
     imshow("contours", cnt_img);
 }
 
@@ -27,35 +34,72 @@ static void on_trackbar(int, void*)
 
 int main( int argc, char** argv)
 {
-
+#ifdef SIMULATION_ONLY
     Mat img = Mat::zeros(w, w, CV_8UC1);
-    //Draw 6 faces
-    for( int i = 0; i < 1; i++ )
+    for (int i = 0; i < 1; i++)
     {
-        int dx = (i%2)*250 - 30;
-        int dy = (i/2)*150;
+        int dx = (i % 2) * 250 - 30;
+        int dy = (i / 2) * 150;
         const Scalar white = Scalar(255);
         const Scalar black = Scalar(0);
-        
-        /*
-        ellipse( img, Point(dx+150, dy+100), Size(100,70), 0, 0, 360, white, -1, 8, 0 );
-        */
+
         rectangle(img, Point(50, 50), Point(100, 100), white, -1);
         rectangle(img, Point(10, 70), Point(210, 90), white, -1);
         rectangle(img, Point(150, 50), Point(200, 100), white, -1);
-
- /*     ellipse( img, Point(dx+115, dy+70), Size(30,20), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+185, dy+70), Size(30,20), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+115, dy+70), Size(15,15), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+185, dy+70), Size(15,15), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+115, dy+70), Size(5,5), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+185, dy+70), Size(5,5), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+150, dy+100), Size(10,5), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+150, dy+150), Size(40,10), 0, 0, 360, black, -1, 8, 0 );
-        ellipse( img, Point(dx+27, dy+100), Size(20,35), 0, 0, 360, white, -1, 8, 0 );
-        ellipse( img, Point(dx+273, dy+100), Size(20,35), 0, 0, 360, white, -1, 8, 0 );*/
     }
-    //show the faces
+#else
+    IMap2D*              m_iMap = nullptr;
+    PolyDriver           m_pMap;
+
+    //open the map interface
+    yarp::os::Property map_options;
+    map_options.put("device", "map2DClient");
+    map_options.put("local", std::string("/area") + "/map2DClient");
+    map_options.put("remote", "/mapServer");
+    if (m_pMap.open(map_options) == false)
+    {
+        yError() << "Unable to open map2DClient";
+        return false;
+    }
+    m_pMap.view(m_iMap);
+    if (m_iMap == 0)
+    {
+        yError() << "Unable to open map interface";
+        return false;
+    }
+
+    //get the map
+    MapGrid2D the_map;
+    std::string map_name = "testMap";
+    bool b = m_iMap->get_map(map_name, the_map);
+    if (b == false)
+    {
+        yError() << "Map not found";
+       return false;
+    }
+
+    yarp::sig::ImageOf<yarp::sig::PixelMono> the_grid;
+    bool b2= the_map.getOccupancyGrid(the_grid);
+
+    size_t w = the_map.width();
+    size_t h = the_map.height();
+    Mat img = Mat::zeros(w,h, CV_8UC1);
+    for (auto x=0; x<w; x++)
+        for (auto y = 0; y <h; y++)
+        {
+            unsigned char c = 0;
+            yarp::dev::MapGrid2D::map_flags pixin;
+            the_map.getMapFlag(yarp::dev::MapGrid2D::XYCell(x, y), pixin);
+            if      (pixin == yarp::dev::MapGrid2D::MAP_CELL_WALL) { c = 0;}
+            else if (pixin == yarp::dev::MapGrid2D::MAP_CELL_UNKNOWN) { c = 0; }
+            else if (pixin == yarp::dev::MapGrid2D::MAP_CELL_FREE) { c = 255; }
+            else if (pixin == yarp::dev::MapGrid2D::MAP_CELL_KEEP_OUT) { c = 255; }
+            else if (pixin == yarp::dev::MapGrid2D::MAP_CELL_ENLARGED_OBSTACLE) { c = 255;   }
+            else if (pixin == yarp::dev::MapGrid2D::MAP_CELL_TEMPORARY_OBSTACLE) { c = 255;  }
+            img.at<unsigned char>(x, y) = c;
+        }
+#endif
+
     namedWindow( "image", 1 );
     imshow( "image", img );
     //Extract the contours so that
@@ -65,12 +109,25 @@ int main( int argc, char** argv)
     for (size_t k = 0; k < contours0.size(); k++)
     {
         yarp::dev::Map2DArea area;
+        double accuracy = 4;
+        approxPolyDP(Mat(contours0[k]), contours[k], accuracy, true);
+#if SIMULATION_ONLY
         area.map_id = "test";
-        approxPolyDP(Mat(contours0[k]), contours[k], 4, true);
+#else
+        area.map_id = the_map.getMapName();
+        double resolution = 1.0; the_map.getResolution(resolution);
+#endif
         for (size_t kk = 0; kk < contours[k].size(); kk++)
         {
-            cout << k << " " << kk << " x:" << contours[k][kk].x << " y:" << contours[k][kk].y << endl;
+#if SIMULATION_ONLY
             area.points.push_back(yarp::math::Vec2D<double>(contours[k][kk].x, contours[k][kk].y));
+            cout << k << " " << kk << " x:" << contours[k][kk].x << " y:" << contours[k][kk].y << endl;
+#else
+            double xx = contours[k][kk].x*resolution;
+            double yy = contours[k][kk].y*resolution;
+            area.points.push_back(yarp::math::Vec2D<double>(xx, yy));
+            cout << k << " " << kk << " x:" << contours[k][kk].x << "(" << xx << ") y:" << contours[k][kk].y << " (" << yy << ")"<<endl;
+#endif
         }
     }
 
