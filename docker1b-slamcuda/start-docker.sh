a0ea3c9a282f50a8c8ca693c070360bbb533945f
Marco Randazzo <marco.randazzo@iit.it>
2020-10-19 10:56:24 +0200

    removed docker stuff

--- docker1b-slamcuda/start-docker.sh
+++
@@ -1,3 +0,0 @@
-#docker run --rm -it --gpus all -e QT_X11_NO_MITSHM=1 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --hostname dockerpc randaz81/r1slam:cuda
-nvidia-docker run --rm -it --gpus all -e QT_X11_NO_MITSHM=1 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics --hostname dockerpc randaz81/r1slam:cuda
-
--- docker1b-slamcuda/start-jupyter.sh
+++
@@ -1,1 +0,0 @@
-cd /workspace && jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' &
