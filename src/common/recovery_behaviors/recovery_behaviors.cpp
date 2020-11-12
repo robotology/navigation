650a2ce1adf6824690023793a0e7b39d61569ad7
Marco Randazzo <marco.randazzo@iit.it>
2020-11-12 11:09:43 +0100

    Added recovery_behaviors (preliminary) Improved CMakeLists to export navigation_lib and make the navigation repo usable with find_package

---
+++ src/common/recovery_behaviors/recovery_behaviors.cpp
@@ -0,0 +1,70 @@
+/*
+•   Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+•   All rights reserved.
+•
+•   This software may be modified and distributed under the terms of the
+•   GPL-2+ license. See the accompanying LICENSE file for details.
+*/
+
+#define _USE_MATH_DEFINES
+#include "recovery_behaviors.h"
+#include <yarp/math/Math.h>
+
+using namespace yarp::os;
+using namespace yarp::dev;
+using namespace yarp::dev::Nav2D;
+
+#ifndef RAD2DEG
+#define RAD2DEG 180/M_PI
+#endif
+
+#ifndef DEG2RAD
+#define DEG2RAD M_PI/180
+#endif
+
+
+//////////////////////////
+recovery_behavior::recovery_behavior(BufferedPort<yarp::os::Bottle>* command_port,
+    IRangefinder2D* iLas ,
+    ILocalization2D* m_iLoc,
+    IMap2D* m_iMap) : PeriodicThread (0.010)
+{
+    m_port_commands_output = command_port;
+}
+
+void recovery_behavior::run()
+{
+    double current_time = yarp::os::Time::now();
+    Bottle& b = m_port_commands_output->prepare();
+    //m_port_commands_output.setEnvelope(stamp);
+    b.clear();
+    b.addInt(2);               // polar speed commands
+    b.addDouble(0.0);          // angle in deg
+    b.addDouble(0.0);          // lin_vel in m/s
+    b.addDouble(ang_speed);    // ang_vel in deg/s
+    b.addDouble(100);
+    m_port_commands_output->write();
+
+    if (current_time-start_time >10.0)
+    {
+         status_completed=true;
+    }
+}
+
+bool recovery_behavior::is_status_completed()
+{
+    return status_completed;
+}
+
+bool recovery_behavior::threadInit()
+{
+    start_time =yarp::os::Time::now();
+    double ang_speed= 10.0;
+    //this->setPeriod(0.010);
+    status_completed=false;
+    return true;
+}
+
+void recovery_behavior::threadRelease()
+{
+}
---
+++ src/common/recovery_behaviors/recovery_behaviors.h
@@ -0,0 +1,64 @@
+/*
+•   Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+•   All rights reserved.
+•
+•   This software may be modified and distributed under the terms of the
+•   GPL-2+ license. See the accompanying LICENSE file for details.
+*/
+
+#include <yarp/os/Network.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/Time.h>
+#include <yarp/os/LogStream.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/Map2DLocation.h>
+#include <yarp/dev/OdometryData.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/PeriodicThread.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/dev/IFrameTransform.h>
+#include <yarp/dev/ILocalization2D.h>
+#include <yarp/dev/IRangefinder2D.h>
+#include <yarp/dev/IMap2D.h>
+#include <mutex>
+#include <math.h>
+#include <iCub/ctrl/adaptWinPolyEstimator.h>
+
+class generic_recovery_behaviour
+{
+    protected:
+    yarp::os::BufferedPort<yarp::os::Bottle>* m_port_commands_output = nullptr;
+    yarp::dev::IRangefinder2D* m_iLaser = nullptr;
+    yarp::dev::Nav2D::ILocalization2D* m_iLoc = nullptr;
+    yarp::dev::Nav2D::IMap2D* m_iMap = nullptr;
+
+    protected:
+    bool status_completed;
+    virtual bool is_status_completed() = 0;
+};
+
+class recovery_behavior : 
+    protected generic_recovery_behaviour,
+    public yarp::os::PeriodicThread 
+{
+private:
+
+    double ang_speed;
+    double start_time;
+
+private:
+    //methods inherited from yarp::os::RateThread
+    virtual void run() override;
+    virtual bool threadInit() override;
+    virtual void threadRelease() override;
+
+public:
+    recovery_behavior(yarp::os::BufferedPort<yarp::os::Bottle>* command_port,
+                      yarp::dev::IRangefinder2D* iLas = nullptr,
+                      yarp::dev::Nav2D::ILocalization2D* m_iLoc = nullptr,
+                      yarp::dev::Nav2D::IMap2D* m_iMap = nullptr);
+    virtual ~recovery_behavior() {};
+
+    bool is_status_completed() override;
+};
