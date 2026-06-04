c6d5a85781db358105c2e1c85860c014bf57eb49
Stefano Berti <berti.ste@hotmail.it>
2026-06-04 11:18:12 +0200

    Adapt joyServer2vel for Meta Quest input

--- src/portmonitors/joystickServer2velocityCommand/joystickServer2velocityCommand.cpp
+++ src/portmonitors/joystickServer2velocityCommand/joystickServer2velocityCommand.cpp
@@ -16,10 +16,17 @@
 #include <yarp/os/Value.h>
 #include <yarp/os/Bottle.h>
 
-//example
-//yarp connect /joystickCtrl:o /baseControl /input /joystick:i tcp+recv.portmonitor+type.dll+file.joyServer2vel
+// Example:
+// yarp connect /joystickCtrl:o /baseControl /input /joystick:i
+//   tcp+recv.portmonitor+type.dll+file.joyServer2vel
 
 YARP_LOG_COMPONENT(JOY2VEL, "navigation.Joy2Vel")
+
+namespace
+{
+constexpr int metaquest_bottle_size = 4;
+constexpr double metaquest_vtheta_scale = 30.0;
+}
 
 bool isNumeric(yarp::os::Value& val)
 {
@@ -57,27 +64,23 @@ bool JoyServer2vel::validate_bot(const yarp::os::Bottle* bot)
         return false;
     }
 
-    if (bot->size() < 5)
+    if (bot->size() != metaquest_bottle_size)
     {
-        yCError(JOY2VEL, "Invalid bottle format: Size <5 or invalid data type");
+        yCError(JOY2VEL, "Invalid Meta Quest bottle format: expected 4 values");
         return false;
     }
 
-    if ( isNumeric(bot->get(0))  == false ||
-         bot->get(1).isFloat64() == false ||
-         bot->get(2).isFloat64() == false ||
-         bot->get(3).isFloat64() == false ||
-         bot->get(4).isFloat64() == false )
+    // Meta Quest messages are expected as:
+    // [left_x, left_y, right_x, right_y]
+    for (int i = 0; i < metaquest_bottle_size; ++i)
     {
-        yCError(JOY2VEL, "Invalid bottle format: invalid data type");
-        return false;
+        yarp::os::Value value = bot->get(i);
+        if (!isNumeric(value))
+        {
+            yCError(JOY2VEL, "Invalid Meta Quest bottle format: item %d is not numeric", i);
+            return false;
+        }
     }
-
-    //when the joystick button is not pressed, filter out the message.
-    //This will eventually trigger a timeout on the receiver and
-    //the control will be give to a different input source
-    double percent = bot->get(4).asFloat64();
-    if (percent < 10) {return false;}
 
     return true;
 }
@@ -88,22 +91,13 @@ yarp::os::Things& JoyServer2vel::update(yarp::os::Things& thing)
     yAssert(bot);
     validate_bot(bot);
 
-    if (bot->get(0).asInt32()==3)
-    {
-        double percent = bot->get(4).asFloat64()/100.0;
-        this->m_command.vel_x = bot->get(1).asFloat64() * percent;
-        this->m_command.vel_y = bot->get(2).asFloat64() * percent;
-        this->m_command.vel_theta = bot->get(3).asFloat64() * percent;
-    } else if (bot->get(0).asInt32()==2)
-    {
-        double percent = bot->get(4).asFloat64()/100.0;
-        this->m_command.vel_x = bot->get(2).asFloat64() * percent  *  cos(bot->get(1).asFloat64()/ 180 * M_PI)  ;
-        this->m_command.vel_y = bot->get(2).asFloat64() * percent * sin(bot->get(1).asFloat64() / 180 * M_PI);
-        this->m_command.vel_theta = bot->get(3).asFloat64() * percent;
-    } else
-    {
-        yCError(JOY2VEL, "Unsupported bottle format: Type!=3");
-    }
+    const double left_x = bot->get(0).asFloat64();
+    const double left_y = bot->get(1).asFloat64();
+    const double right_x = bot->get(2).asFloat64();
+
+    this->m_command.vel_x = left_y;
+    this->m_command.vel_y = left_x;
+    this->m_command.vel_theta = -right_x * metaquest_vtheta_scale;
 
     return this->m_things;
 }
